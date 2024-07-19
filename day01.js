1fcb54b18bf46194f550b16a85e22d9cfd72f470
harshkumar1302 <bs824536@gmail.com>
2024-07-19 18:03:20 +0530

    first commit

---
+++ day01.js
@@ -0,0 +1,60 @@
+
+// // Activity 1 : Variable Declaration
+
+
+// // Task 1
+// var numbers = 25;
+// console.log(numbers);
+
+// // Task 2
+// let char = "Hello";
+// console.log(char);
+
+
+// // Activity 2 : Constant Declaration
+
+
+// // Task 3
+// const ans = true;
+// console.log(ans);
+
+
+// // Activity 3 : Data Types
+
+
+// // Task 4
+// var num = 23;
+// var char = "HI";
+// var ans = false;
+// var details = {
+//     name: "Harsh",
+//     age: 25,
+//     gender: "Male"
+// }
+// var arr = [1, 2, 5, 9, 7];
+
+// console.log(typeof (num));
+// console.log(typeof (char));
+// console.log(typeof (ans));
+// console.log(typeof (details));
+// console.log(typeof (arr));
+
+
+// // Activity 4 : Reassigning Variables
+
+
+// // Task 5
+// let nums = 34;
+// console.log(nums);
+// nums = 45;
+// console.log(nums);
+
+
+// // Activity 5 : Understanding const
+
+
+// // Task 6
+// const reassign = 56;
+// console.log(reassign);
+// reassign = 66;
+// console.log(reassign);
